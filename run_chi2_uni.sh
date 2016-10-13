db3d43d0e9f54d9cae2606150b73ac06b99e6d0f
kn45 <381404825@qq.com>
2016-10-14 01:03:33 +0800

    update example

--- run_chi2_uni.sh
+++ run_chi2_uni.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-job_name=kn45_chi_square
-
 echo "*******`date` start: $0 $* *******"
 
+JOB_NAME=kn45_chi_square
 INPUT_PATH_T=/user/kn45/chi2_segs_uni
 OUTPUT_PATH=/user/kn45/chi2_uni
 
@@ -12,7 +11,7 @@ hadoop fs -rmr $OUTPUT_PATH
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.4.0.jar \
 -input $INPUT_PATH_T -output $OUTPUT_PATH \
 -mapper "mapred_chi2.py m" -reducer "mapred_chi2.py r all_cat_segs_cnt" \
--jobconf mapred.map.tasks=400 -jobconf mapred.reduce.tasks=300 -jobconf mapred.job.name=$job_name \
+-jobconf mapred.map.tasks=400 -jobconf mapred.reduce.tasks=300 -jobconf mapred.job.name=$JOB_NAME \
 -file mapred_chi2.py all_cat_segs_cnt
 
 TABLE_NAME=kn45_chi2_uni
